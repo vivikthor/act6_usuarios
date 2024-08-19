2351cbcf06f124f643d4fc62e739b56ab2e87c9c
Víctor <Vivivikthor@gmail.com>
2024-08-19 20:36:15 +0200

    Implementación de sweetAlert2

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, Input } from '@angular/core';
 import { IUser } from '../../interfaces/iuser.interface';
 import { RouterLink } from '@angular/router';
 import { UsersServiceService } from '../../services/users-service.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-user-card',
@@ -12,19 +13,47 @@ import { UsersServiceService } from '../../services/users-service.service';
 })
 export class UserCardComponent {
   @Input() myUser?: IUser;
-  userService = inject(UsersServiceService)
+  userService = inject(UsersServiceService);
 
-
-  async delete(_id: string | undefined) {
+  async delete(_id: string | undefined){
     if (_id) {
       try {
         const resDelete: any = await this.userService.delete(_id);
-        console.log(resDelete);
 
+        const swalWithBootstrapButtons = Swal.mixin({
+          customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger',
+          },
+          buttonsStyling: false,
+        });
+        swalWithBootstrapButtons
+          .fire({
+            title: `¿Quieres eliminar a ${this.myUser?.first_name}?`,
+            text: 'Si borras al usuario no habrá vuelta atrás',
+            // icon: 'question',
+            imageUrl: this.myUser?.image,
+            imageWidth: 200,
+            imageHeight: 200,
+            imageAlt: `Foto de perfil de ${this.myUser?.image}`,
+            showCancelButton: true,
+            confirmButtonText: '¡Sí, borrar!',
+            cancelButtonText: '¡No, no quiero!',
+            reverseButtons: true,
+          })
+          .then((result) => {
+            if (result.isConfirmed) {
+              swalWithBootstrapButtons.fire({
+                title: '¡Borrado!',
+                text: 'El usuario ha sido eliminado',
+                icon: 'success',
+              });
+              console.log(resDelete);
+            }
+          });
       } catch (error) {
         console.log(error);
       }
     }
-
   }
 }
